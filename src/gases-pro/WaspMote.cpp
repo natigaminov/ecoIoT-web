86abc5b375545ca3d3ecd1b61146f69bbf695ee0
Andrew Laptev <nickname.avast@gmail.com>
2021-12-08 16:28:08 +0300

    Added converting ppm and LEL to mg/m3 in convGasValues() method. Also added method wrapper getMeasurements() and comments

--- src/gases-pro/WaspMote.cpp
+++ src/gases-pro/WaspMote.cpp
@@ -2,6 +2,9 @@
 
 WaspMote::WaspMote(){}
 
+/*
+ * Инициализация всех сокетов для сенсоров
+ */
 void WaspMote::turnGasON() {
   #ifdef MODEL_GREEN
   gas_sensor_CO.ON();
@@ -15,6 +18,9 @@ void WaspMote::turnGasON() {
   #endif
 }
 
+/*
+ * Деинициализация всех сокетов на сенсоры
+ */
 void WaspMote::turnGasOFF() {
   #ifdef MODEL_GREEN
   gas_sensor_CO.OFF();
@@ -28,27 +34,62 @@ void WaspMote::turnGasOFF() {
   #endif
 }
 
-void WaspMote::getGasValues() {
+/*
+ * Конвертация значений сенсоров из ppm в мг/м3 и перевод в строковый тип
+ * Данный метод класса WaspMote является protected, т.к. идет в жесткой связке после метода
+ * getMeteoValues() и не должен быть вызван отдельно во избежание некорректных
+ * значений. 
+ * Для вызова данного метода в связке с getMeteoValues() используется
+ * метод-враппер getMeasurements()
+ */
+void WaspMote::convGasValues() {
+  float coeff1 = 0.12 * 0.001 * press;
+  float coeff2 = (273 + temp);
   #ifdef MODEL_GREEN
-  Utils.float2String(gas_sensor_CO.getConc(), concentration_CO, 10);
-  Utils.float2String(gas_sensor_NO.getConc(), concentration_NO, 10);
-  Utils.float2String(gas_sensor_CO2.getConc(), concentration_CO2, 10);
-  Utils.float2String(gas_sensor_NO2.getConc(), concentration_NO2, 10);
+  conv_CO = coeff1 * (gas_sensor_CO.getConc() * 28.01) / coeff2;
+  conv_NO = coeff1 * (gas_sensor_NO.getConc() * 30.01) / coeff2;
+  conv_CO2 = coeff1 * (gas_sensor_NO.getConc() * 44.01) / coeff2;
+  conv_NO2 = coeff1 * (gas_sensor_NO.getConc() * 46.0055) / coeff2;
+  Utils.float2String(conv_CO, concentration_CO, 10);
+  Utils.float2String(conv_NO, concentration_NO, 10);
+  Utils.float2String(conv_CO2, concentration_CO2, 10);
+  Utils.float2String(conv_NO2, concentration_NO2, 10);
   #elif defined MODEL_BLUE
-  Utils.float2String(gas_sensor_NH3.getConc(), concentration_NH3, 10);
-  Utils.float2String(gas_sensor_H2S.getConc(), concentration_H2S, 10);
-  Utils.float2String(gas_sensor_CH4.getConc(), concentration_CH4, 10);
+  conv_NH3 = coeff1 * (gas_sensor_NH3.getConc() * 17.031) / coeff2;
+  conv_H2S = coeff1 * (gas_sensor_H2S.getConc() * 34.1) / coeff2;
+  conv_CH4 = coeff1 * (10000 * gas_sensor_CH4.getConc() * 16.04) / coeff2;
+  Utils.float2String(conv_NH3, concentration_NH3, 10);
+  Utils.float2String(conv_H2S, concentration_H2S, 10);
+  Utils.float2String(conv_CH4, concentration_CH4, 10);
   #endif
 }
 
+/*
+ * Измерение значений
+ */
+void WaspMote::getMeasurements() {
+  getMeteoValues();
+  convGasValues();
+}
+
+/*
+ * Инициализация сенсора температуры, влажности и давления BME280
+ */
 void WaspMote::turnMeteON() {
   bme.ON();
 }
 
+/*
+ * Получение данных с сенсора BME280
+ */
 void WaspMote::getMeteoValues() {
-  Utils.float2String(bme.getTemperature(), temperature, 6);
-  Utils.float2String(bme.getHumidity(), humidity, 6);
-  Utils.float2String(bme.getPressure(), pressure, 6);
+  temp = bme.getTemperature();
+  hum = bme.getHumidity();
+  press = bme.getPressure();
+  Utils.float2String(temp, temperature, 6);
+  Utils.float2String(hum, humidity, 6);
+  Utils.float2String(press, pressure, 6);
 }
 
+// создаем объект класса заранее
 WaspMote WPM = WaspMote();
--- src/gases-pro/WaspMote.h
+++ src/gases-pro/WaspMote.h
@@ -4,6 +4,8 @@
 #include "toggle.h"
 
 class WaspMote {
+  protected:
+    void convGasValues();
   public:
     #ifdef MODEL_GREEN
     Gas gas_sensor_CO{SOCKET_A};
@@ -14,6 +16,7 @@ class WaspMote {
     char concentration_NO[20];
     char concentration_CO2[20];
     char concentration_NO2[20];
+    float conv_CO, conv_NO, conv_CO2, conv_NO2;
     #elif defined MODEL_BLUE
     Gas gas_sensor_NH3{SOCKET_A};
     Gas gas_sensor_H2S{SOCKET_B};
@@ -21,18 +24,21 @@ class WaspMote {
     char concentration_NH3[20];
     char concentration_H2S[20];
     char concentration_CH4[20];
+    float conv_NH3, conv_H2S, conv_CH4;
     #endif
     bmeGasesSensor bme;
     char temperature[20];
     char humidity[20];
     char pressure[20];
+    float temp, hum, press;
 
     WaspMote();
     void turnGasON();
     void turnGasOFF();
-    void getGasValues();
     void turnMeteON();
     void getMeteoValues();
+    void getMeasurements();
 };
 
+// объявляем внешнее имя заранее созданного объекта класса
 extern WaspMote WPM;
