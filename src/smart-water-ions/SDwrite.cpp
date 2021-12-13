4537f63a043a0e8bb7a602c79c1f3e29b825a3d3
Andrew Laptev <nickname.avast@gmail.com>
2021-12-13 17:38:07 +0300

    Added creation file in folder

--- src/smart-water-ions/SDwrite.cpp
+++ src/smart-water-ions/SDwrite.cpp
@@ -13,11 +13,14 @@ SDwrite::SDwrite(){}
 
 void SDwrite::createFile() {
   RTC.getTime();
-  sprintf(filename, "%02u%02u%02u%02u.csv", RTC.month, RTC.date, RTC.hour, RTC.minute);
-  if(SD.create(filename)) {
+  sprintf(foldername, "%02u-%02u-%02u", RTC.year, RTC.month, RTC.date);
+  SD.mkdir(foldername);
+  sprintf(filename, "%02u-%02u-%02u.csv", RTC.hour, RTC.minute, RTC.second);
+  sprintf(filepath, "%s/%s", foldername, filename);
+  if(SD.create(filepath)) {
     USB.print(F("File created: "));
-    USB.println(filename);
-    sd_answer = SD.appendln(filename, "Time,Ammonium,Nitrite,Nitrate");
+    USB.println(filepath);
+    sd_answer = SD.appendln(filepath, "Time,Ammonium,Nitrite,Nitrate");
     if(sd_answer == 1){
       USB.println(F("\nData saved"));
     }else{
@@ -31,15 +34,15 @@ void SDwrite::createFile() {
 void SDwrite::writeToFile(char Ammonium[26], char Nitrite[26], char Nitrate[26]) {
   RTC.getTime();
   sprintf(writetime, "20%02u-%02u-%02u %02u:%02u:%02u", RTC.year, RTC.month, RTC.date, RTC.hour, RTC.minute, RTC.second);
-  SD.append(filename, writetime);
-  SD.append(filename, ",");
-  SD.append(filename, Ammonium);
-  SD.append(filename, ",");
-  SD.append(filename, Nitrite);
-  SD.append(filename, ",");
-  SD.appendln(filename, Nitrate);
+  SD.append(filepath, writetime);
+  SD.append(filepath, ",");
+  SD.append(filepath, Ammonium);
+  SD.append(filepath, ",");
+  SD.append(filepath, Nitrite);
+  SD.append(filepath, ",");
+  SD.appendln(filepath, Nitrate);
 
-  // SD.showFile(filename);
+  // SD.showFile(filepath);
 }
 
 SDwrite SDW = SDwrite();
