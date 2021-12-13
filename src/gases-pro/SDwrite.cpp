62245442b141efdb4f060dcb351ad746efac60a3
Andrew Laptev <nickname.avast@gmail.com>
2021-12-13 17:52:07 +0300

    Added creation file in folder

--- src/gases-pro/SDwrite.cpp
+++ src/gases-pro/SDwrite.cpp
@@ -13,20 +13,23 @@ SDwrite::SDwrite(){}
 
 void SDwrite::createFile() {
   RTC.getTime();
-  sprintf(filename, "%02u%02u%02u%02u.csv", RTC.month, RTC.date, RTC.hour, RTC.minute);
-  if(SD.create(filename)) {
+  sprintf(foldername, "%02u-%02u-%02u", RTC.year, RTC.month, RTC.date);
+  SD.mkdir(foldername);
+  sprintf(filename, "%02u-%02u-%02u.csv", RTC.hour, RTC.minute, RTC.second);
+  sprintf(filepath, "%s/%s", foldername, filename);
+  if(SD.create(filepath)) {
     USB.print(F("File created: "));
-    USB.println(filename);
-    sd_answer = SD.append(filename, "Time,Temp,Hum,Press,");
+    USB.println(filepath);
+    sd_answer = SD.append(filepath, "Time,Temp,Hum,Press,");
     if(sd_answer == 1){
       USB.println(F("\nData saved"));
     }else{
       USB.println(F("\nAppend data error!"));
     }
     #ifdef MODEL_GREEN
-    SD.appendln(filename, "CO,NO,CO2,NO2");
+    SD.appendln(filepath, "CO,NO,CO2,NO2");
     #elif defined MODEL_BLUE
-    SD.appendln(filename, "NH3,H2S,CH4");
+    SD.appendln(filepath, "NH3,H2S,CH4");
     #endif
   }else{
     USB.println(F("File NOT created"));
@@ -36,31 +39,31 @@ void SDwrite::createFile() {
 void SDwrite::writeToFile() {
   RTC.getTime();
   sprintf(writetime, "20%02u-%02u-%02u %02u:%02u:%02u", RTC.year, RTC.month, RTC.date, RTC.hour, RTC.minute, RTC.second);
-  SD.append(filename, writetime);
-  SD.append(filename, ",");
-  SD.append(filename, WPM.temperature);
-  SD.append(filename, ",");
-  SD.append(filename, WPM.humidity);
-  SD.append(filename, ",");
-  SD.append(filename, WPM.pressure);
-  SD.append(filename,",");
+  SD.append(filepath, writetime);
+  SD.append(filepath, ",");
+  SD.append(filepath, WPM.temperature);
+  SD.append(filepath, ",");
+  SD.append(filepath, WPM.humidity);
+  SD.append(filepath, ",");
+  SD.append(filepath, WPM.pressure);
+  SD.append(filepath,",");
   #ifdef MODEL_GREEN
-  SD.append(filename, WPM.concentration_CO);
-  SD.append(filename,",");
-  SD.append(filename, WPM.concentration_NO);
-  SD.append(filename,",");
-  SD.append(filename, WPM.concentration_CO2);
-  SD.append(filename,",");
-  SD.appendln(filename, WPM.concentration_NO2);
+  SD.append(filepath, WPM.concentration_CO);
+  SD.append(filepath,",");
+  SD.append(filepath, WPM.concentration_NO);
+  SD.append(filepath,",");
+  SD.append(filepath, WPM.concentration_CO2);
+  SD.append(filepath,",");
+  SD.appendln(filepath, WPM.concentration_NO2);
   #elif defined MODEL_BLUE
-  SD.append(filename, WPM.concentration_NH3);
-  SD.append(filename,",");
-  SD.append(filename, WPM.concentration_H2S);
-  SD.append(filename,",");
-  SD.appendln(filename, WPM.concentration_CH4);
+  SD.append(filepath, WPM.concentration_NH3);
+  SD.append(filepath,",");
+  SD.append(filepath, WPM.concentration_H2S);
+  SD.append(filepath,",");
+  SD.appendln(filepath, WPM.concentration_CH4);
   #endif
 
-  SD.showFile(filename);
+  SD.showFile(filepath);
 }
 
 SDwrite SDW = SDwrite();
--- src/gases-pro/SDwrite.h
+++ src/gases-pro/SDwrite.h
@@ -9,6 +9,8 @@ class SDwrite {
   protected:
     uint8_t sd_answer;
     char filename[13];
+    char foldername[13];
+    char filepath[26];
     char writetime[20];
   public:
     SDwrite();
