287567f9e4610bef3a48262c8386776f02d05418
Andrew Laptev <nickname.avast@gmail.com>
2022-10-25 00:18:55 +0300

    refactor: update comments

--- src/server/app/repositories/measure.py
+++ src/server/app/repositories/measure.py
@@ -12,6 +12,9 @@ from app.libs.postgresql.gases_database.models import (
 
 class MeasureRepository:
     def __init__(self, sessions: dict[str, sessionmaker] = Depends(psql.get_sessionmakers)) -> None:
+        """Через инъекцию зависимостей передается словарь с фабриками сессий `sessions` со всех БД
+        Вызвать фабрику сессий из этого словаря можно по имени БД
+        """
         self.sessions = sessions
         self.gases_db_name = settings.gases_database_name
     
