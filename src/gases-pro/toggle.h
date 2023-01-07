1a9096255c7703effa8464babf0a063970afa4ee
Natig Aminov <natig.aminov@gmail.com>
2023-01-08 00:08:43 +0300

    Delete firmware files

--- src/gases-pro/toggle.h
+++
@@ -1,9 +0,0 @@
-/* 
- * Для переключения модуля нужно поменять объявления макросов
- */
-
-#if true // чтобы было MODEL_BLUE надо поставить false 
-    #define MODEL_GREEN
-#else
-    #define MODEL_BLUE
-#endif
